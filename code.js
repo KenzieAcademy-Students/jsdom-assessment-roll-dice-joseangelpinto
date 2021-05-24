91f228ed7b51d88fa4662df4349465aa4a73ecc5
kenzieadmin <kenzieadmin@users.noreply.github.com>
2021-05-24 02:53:04 +0000

    deploy: e839011d0ba3e019ccc714a19c697b8949daae80

--- code.js
+++ code.js
@@ -2,3 +2,17 @@ let results = [null, null, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 // Your Code Here!
 
+function rollDie () { 
+    return Math.ceil (Math.random ()* 6)
+
+    for(let i = 0; i <5; i ++) {
+    console.log (rollDie (1-6)) 
+ 
+
+    }
+    
+  
+}
+  
+let diceResult = 6;
+results[diceResult] += 1;
